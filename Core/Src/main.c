732bdaa1cbcc51ab79398d9c7f769b9cde7fe126
Sergey Ryzhikov <sergey.ryzhikov@ihep.ru>
2021-10-01 02:10:17 +0300

    UDP echo server

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -24,7 +24,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "lwip/inet.h"
+#include "lwip/sockets.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -1226,9 +1227,50 @@ void StartDefaultTask(void *argument)
   /* init code for LWIP */
   MX_LWIP_Init();
   /* USER CODE BEGIN 5 */
+
+  const unsigned SERVER_PORT = 5000;
+  struct sockaddr_in server_address;
+
+  memset(&server_address, 0, sizeof(server_address));
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(SERVER_PORT);
+  server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  int sock;
+  if ((sock = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
+    printf("could not create socket\n");
+    return;
+  }
+
+  if ((bind(sock, (struct sockaddr *)&server_address,
+	    sizeof(server_address))) < 0) {
+    printf("could not bind socket\n");
+    return;
+  }
+
+
+  struct sockaddr_in client_address;
+  socklen_t client_address_len = 0;
+
+  char buffer[500];
+  char * ptr;
+
+  printf("start loop \n\r");
   /* Infinite loop */
   for(;;)
   {
+    client_address_len = sizeof(client_address);
+    int len = recvfrom(sock, buffer, sizeof(buffer), 0,
+		 (struct sockaddr *)&client_address,
+		 &client_address_len);
+    buffer[len] = '\0';
+
+    printf("\n\r received: '%s' %d from client %s\n", buffer, len,
+    	       inet_ntoa(client_address.sin_addr));
+    // echo
+    sendto(sock, buffer, len, 0, (struct sockaddr *)&client_address,
+	       sizeof(client_address));
+
     osDelay(1);
   }
   /* USER CODE END 5 */
